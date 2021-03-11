c5e3ef4625dfacaa2380c14bb9ee4459a586f2ee
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-03-11 15:04:04 +0200

    Revert "asda"
    This reverts commit 8d8f10cb

--- app/src/main/java/com/juuh/ht/MainActivity.java
+++ app/src/main/java/com/juuh/ht/MainActivity.java
@@ -15,7 +15,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void helloworld(View v){
         System.out.println("dxd");
-        System.out.println("asdasdas");
-        System.out.println("asdasdas");
     }
 }
