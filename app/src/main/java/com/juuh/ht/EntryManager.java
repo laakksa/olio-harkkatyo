eb739b443b34bb38cf10a70cc52cc50ca7e7ee5f
sakulaakkonen <saku.laakkonen@student.lut.fi>
2021-04-13 10:39:24 +0300

    minor fixes

--- app/src/main/java/com/juuh/ht/EntryManager.java
+++ app/src/main/java/com/juuh/ht/EntryManager.java
@@ -1,12 +1,18 @@
 package com.juuh.ht;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class EntryManager {
     private ArrayList<TeamEntry> TeamsList;
     private static EntryManager em_instance = null;
     private EntryManager(){
         TeamsList = new ArrayList<TeamEntry>();
+        getTeams();
     }
 
     public static EntryManager getInstance(){
@@ -27,5 +33,37 @@ public class EntryManager {
 
     public ArrayList<TeamEntry> getTeamsList(){
         return TeamsList;
+    }
+
+
+    public void getTeams(){
+        //Get data from kyykkacom API
+        String myurl = "https://kyykka.com/api/teams/?format=json";
+        String result;
+        JSONAsyncTask get = new JSONAsyncTask();
+        try {
+            result = get.execute(myurl).get();
+            JSONArray ja = new JSONArray(result);
+            for (int i = 0; i < ja.length(); i++) {
+                JSONObject j = (JSONObject) ja.get(i);
+                String abbreviation = j.getString("abbreviation");
+                String name = j.getString("name");
+                int id = j.getInt("id");
+                int wins = j.getInt("matches_won");
+                int losses = j.getInt("matches_lost");
+                int ties = j.getInt("matches_tie");
+                int matches_played = j.getInt("matches_played");
+                int points = j.getInt("points_total");
+                TeamEntry teamEntry = new TeamEntry(id, name, wins, losses, ties, matches_played,
+                        points, abbreviation);
+                addTeamEntry(teamEntry);
+            }
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
